3c4f7dbdbe8b3008b6ebdb36cbceb72dc53d0b14
kazuki watanabe <kazukiwatanabe@kazukinoMacBook-Pro.local>
2023-12-23 17:02:48 +0900

    Execute sample graphql request

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
 import { GraphQLClient, RequestOptions } from "graphql-request";
+import { getSdk } from "./generated";
 
 type GraphQLHeaders = RequestOptions["requestHeaders"];
+
+const sdk = getSdk(new GraphQLClient("https://api.github.com/graphql", {}));
+sdk.feed({}, {}).then((data) => {
+  console.log(data);
+});
